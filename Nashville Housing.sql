02b9d1f85a4d3dac71b9005c931037fde8ae7571
Gabriela <136575175+gabrielatomosoiu@users.noreply.github.com>
2024-02-25 22:08:46 +0200

    Add files via upload

--- Nashville Housing.sql
+++ Nashville Housing.sql
@@ -73,3 +73,117 @@ Set PropertySplitCity = SUBSTRING(PropertyAddress, CHARINDEX(',', PropertyAddres
 
 select *
 from PortfolioProject.dbo.NashvilleHousing	
+
+
+select OwnerAddress
+from PortfolioProject.dbo.NashvilleHousing	
+
+
+select
+parsename(REPLACE(owneraddress, ',', '.'), 3),
+parsename(REPLACE(owneraddress, ',', '.'), 2),
+parsename(REPLACE(owneraddress, ',', '.'), 1)
+from PortfolioProject.dbo.NashvilleHousing	
+
+
+alter table NashvilleHousing
+Add OwnerSplitAddress nvarchar(255);
+
+Update NashvilleHousing
+Set OwnerSplitAddress = parsename(REPLACE(owneraddress, ',', '.'), 3)
+
+
+alter table NashvilleHousing
+Add OwnerSplitCity nvarchar(255);
+
+Update NashvilleHousing
+Set OwnerSplitCity = parsename(REPLACE(owneraddress, ',', '.'), 2)
+
+
+alter table NashvilleHousing
+Add OwnerSplitState nvarchar(255);
+
+Update NashvilleHousing
+Set OwnerSplitState = parsename(REPLACE(owneraddress, ',', '.'), 1)
+
+select *
+from PortfolioProject.dbo.NashvilleHousing	
+
+--change Y and N to yes and no in "sold as vacant" field
+
+select distinct(SoldAsVacant), count(soldasvacant)
+from PortfolioProject.dbo.NashvilleHousing	
+group by SoldAsVacant
+order by 2
+
+
+
+select SoldAsVacant,
+case	when SoldAsVacant = 'Y'then 'Yes'
+		when SoldAsVacant = 'N'then 'No'
+		else SoldAsVacant
+end
+from PortfolioProject.dbo.NashvilleHousing	
+
+
+Update NashvilleHousing
+Set SoldAsVacant = case	when SoldAsVacant = 'Y'then 'Yes'
+		when SoldAsVacant = 'N'then 'No'
+		else SoldAsVacant
+end
+
+--remove duplicates -----------> not to raw data
+
+select *,
+	ROW_NUMBER() over (
+	partition by parcelID,
+				propertyaddress,
+				saleprice,
+				saledate,
+				legalreference
+				order by uniqueID
+				) row_num
+from PortfolioProject.dbo.NashvilleHousing	
+order by ParcelID
+
+--=> CTE =>>> like a temp table
+
+With rownumCTE as(
+select *,
+	ROW_NUMBER() over (
+	partition by parcelID,
+				propertyaddress,
+				saleprice,
+				saledate,
+				legalreference
+				order by uniqueID
+				) row_num
+from PortfolioProject.dbo.NashvilleHousing	
+--order by ParcelID
+)
+select *
+from rownumCTE
+where row_num > 1
+
+--=> delete row num 2
+
+--delete
+--from rownumCTE
+--where row_num > 1
+
+
+
+--delete unused columns -----------> not to raw data
+
+select *
+from PortfolioProject.dbo.NashvilleHousing	
+
+alter table PortfolioProject.dbo.NashvilleHousing
+drop column owneraddress, taxdistrict, propertyaddress
+
+alter table PortfolioProject.dbo.NashvilleHousing
+drop column saledate
+
+
+
+
